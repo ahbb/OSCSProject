f6fe9007d6522af7565c06688655450a87c0b9fc
Unknown <amiir9955@gmail.com>
2018-05-21 11:23:05 +0800

    adding template

---
+++ OSCS/Main/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Template
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Template()); // Start Point, Change this to your Window Form Name to make it Run.
+
+        }
+    }
+}
