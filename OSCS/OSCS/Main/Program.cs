c8c1d8dc012a506bc2f69fbc1accec74301ce2dd
Unknown <amiir9955@gmail.com>
2018-06-14 22:26:21 +0800

    added empty login and reg windows

--- OSCS/OSCS/Main/Program.cs
+++ OSCS/OSCS/Main/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OSCS.WinForms.Login;
+using OSCS.WinForms.Registration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +18,7 @@ namespace OSCS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login()); //change to name of window you want to run
         }
     }
 }
