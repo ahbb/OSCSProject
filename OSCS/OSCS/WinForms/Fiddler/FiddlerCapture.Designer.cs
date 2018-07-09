ad27a8ac91ee9f061465789e09fe6fb4e1c94e53
Unknown <amiir9955@gmail.com>
2018-07-09 22:40:09 +0800

    fiddler window design update
    added anchor

--- OSCS/OSCS/WinForms/Fiddler/FiddlerCapture.Designer.cs
+++ OSCS/OSCS/WinForms/Fiddler/FiddlerCapture.Designer.cs
@@ -40,7 +40,7 @@
             // 
             // tbCapture
             // 
-            this.tbCapture.Location = new System.Drawing.Point(11, 106);
+            this.tbCapture.Location = new System.Drawing.Point(11, 149);
             this.tbCapture.Margin = new System.Windows.Forms.Padding(2);
             this.tbCapture.Name = "tbCapture";
             this.tbCapture.Size = new System.Drawing.Size(65, 27);
@@ -51,7 +51,7 @@
             // 
             // tbStop
             // 
-            this.tbStop.Location = new System.Drawing.Point(91, 106);
+            this.tbStop.Location = new System.Drawing.Point(92, 149);
             this.tbStop.Margin = new System.Windows.Forms.Padding(2);
             this.tbStop.Name = "tbStop";
             this.tbStop.Size = new System.Drawing.Size(65, 27);
@@ -62,7 +62,8 @@
             // 
             // tbClear
             // 
-            this.tbClear.Location = new System.Drawing.Point(724, 106);
+            this.tbClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClear.Location = new System.Drawing.Point(725, 149);
             this.tbClear.Margin = new System.Windows.Forms.Padding(2);
             this.tbClear.Name = "tbClear";
             this.tbClear.Size = new System.Drawing.Size(65, 27);
@@ -73,7 +74,8 @@
             // 
             // tbSave
             // 
-            this.tbSave.Location = new System.Drawing.Point(648, 106);
+            this.tbSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSave.Location = new System.Drawing.Point(646, 149);
             this.tbSave.Margin = new System.Windows.Forms.Padding(2);
             this.tbSave.Name = "tbSave";
             this.tbSave.Size = new System.Drawing.Size(65, 27);
@@ -84,28 +86,33 @@
             // 
             // txtCapture
             // 
-            this.txtCapture.Location = new System.Drawing.Point(-3, 157);
+            this.txtCapture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCapture.Location = new System.Drawing.Point(-3, 194);
             this.txtCapture.Margin = new System.Windows.Forms.Padding(2);
             this.txtCapture.Multiline = true;
             this.txtCapture.Name = "txtCapture";
             this.txtCapture.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtCapture.Size = new System.Drawing.Size(805, 301);
+            this.txtCapture.Size = new System.Drawing.Size(805, 264);
             this.txtCapture.TabIndex = 4;
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(78, 69);
+            this.label1.Location = new System.Drawing.Point(79, 103);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(664, 35);
+            this.label1.Size = new System.Drawing.Size(682, 35);
             this.label1.TabIndex = 5;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(298, 19);
+            this.label2.Location = new System.Drawing.Point(327, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 20);
             this.label2.TabIndex = 6;
