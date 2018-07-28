58281c919eeafd955c90be614d7bcbb26d220c85
Unknown <dixuan99@gmail.com>
2018-07-29 00:32:39 +0800

    File virus checker minor updates

--- OSCS/OSCS/WinForms/FileVirusChecker/FileVirusChecker.Designer.cs
+++ OSCS/OSCS/WinForms/FileVirusChecker/FileVirusChecker.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileVirusChecker));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.Header = new System.Windows.Forms.Panel();
             this.projectTitle = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -58,8 +58,9 @@
             this.Header.BackColor = System.Drawing.Color.MidnightBlue;
             this.Header.Controls.Add(this.projectTitle);
             this.Header.Location = new System.Drawing.Point(0, 0);
+            this.Header.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Header.Name = "Header";
-            this.Header.Size = new System.Drawing.Size(821, 73);
+            this.Header.Size = new System.Drawing.Size(1095, 90);
             this.Header.TabIndex = 17;
             // 
             // projectTitle
@@ -68,11 +69,12 @@
             this.projectTitle.AutoSize = true;
             this.projectTitle.Font = new System.Drawing.Font("Gill Sans MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.projectTitle.ForeColor = System.Drawing.SystemColors.Control;
-            this.projectTitle.Location = new System.Drawing.Point(12, 22);
-            this.projectTitle.MaximumSize = new System.Drawing.Size(250, 30);
-            this.projectTitle.MinimumSize = new System.Drawing.Size(250, 30);
+            this.projectTitle.Location = new System.Drawing.Point(16, 27);
+            this.projectTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.projectTitle.MaximumSize = new System.Drawing.Size(333, 37);
+            this.projectTitle.MinimumSize = new System.Drawing.Size(333, 37);
             this.projectTitle.Name = "projectTitle";
-            this.projectTitle.Size = new System.Drawing.Size(250, 30);
+            this.projectTitle.Size = new System.Drawing.Size(333, 37);
             this.projectTitle.TabIndex = 2;
             this.projectTitle.Text = "Online Chat Protection";
             // 
@@ -81,9 +83,10 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(320, 91);
+            this.label2.Location = new System.Drawing.Point(427, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.Size = new System.Drawing.Size(190, 25);
             this.label2.TabIndex = 18;
             this.label2.Text = "File Virus Checker";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -92,9 +95,10 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 111);
+            this.label1.Location = new System.Drawing.Point(36, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(754, 122);
+            this.label1.Size = new System.Drawing.Size(1005, 170);
             this.label1.TabIndex = 19;
             this.label1.Text = resources.GetString("label1.Text");
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -104,10 +108,10 @@
             this.chooseFile.BackColor = System.Drawing.Color.Honeydew;
             this.chooseFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chooseFile.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.chooseFile.Location = new System.Drawing.Point(76, 251);
-            this.chooseFile.Margin = new System.Windows.Forms.Padding(2);
+            this.chooseFile.Location = new System.Drawing.Point(101, 309);
+            this.chooseFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chooseFile.Name = "chooseFile";
-            this.chooseFile.Size = new System.Drawing.Size(101, 46);
+            this.chooseFile.Size = new System.Drawing.Size(135, 57);
             this.chooseFile.TabIndex = 20;
             this.chooseFile.Text = "Choose a File";
             this.chooseFile.UseVisualStyleBackColor = false;
@@ -117,10 +121,9 @@
             // 
             this.fileName.AutoSize = true;
             this.fileName.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileName.Location = new System.Drawing.Point(73, 330);
-            this.fileName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.fileName.Location = new System.Drawing.Point(97, 406);
             this.fileName.Name = "fileName";
-            this.fileName.Size = new System.Drawing.Size(0, 17);
+            this.fileName.Size = new System.Drawing.Size(0, 20);
             this.fileName.TabIndex = 21;
             this.fileName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -129,10 +132,10 @@
             this.scanFile.BackColor = System.Drawing.Color.Honeydew;
             this.scanFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scanFile.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.scanFile.Location = new System.Drawing.Point(76, 382);
-            this.scanFile.Margin = new System.Windows.Forms.Padding(2);
+            this.scanFile.Location = new System.Drawing.Point(101, 470);
+            this.scanFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.scanFile.Name = "scanFile";
-            this.scanFile.Size = new System.Drawing.Size(101, 46);
+            this.scanFile.Size = new System.Drawing.Size(135, 57);
             this.scanFile.TabIndex = 22;
             this.scanFile.Text = "Scan File";
             this.scanFile.UseVisualStyleBackColor = false;
@@ -141,10 +144,9 @@
             // result
             // 
             this.result.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.result.Location = new System.Drawing.Point(73, 443);
-            this.result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.result.Location = new System.Drawing.Point(97, 545);
             this.result.Name = "result";
-            this.result.Size = new System.Drawing.Size(292, 79);
+            this.result.Size = new System.Drawing.Size(389, 97);
             this.result.TabIndex = 23;
             this.result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -157,9 +159,10 @@
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(605, 233);
+            this.label3.Location = new System.Drawing.Point(807, 287);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 18);
+            this.label3.Size = new System.Drawing.Size(135, 23);
             this.label3.TabIndex = 25;
             this.label3.Text = "Scan Results";
             // 
@@ -170,28 +173,29 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.dataGridView1.Location = new System.Drawing.Point(510, 261);
+            this.dataGridView1.Location = new System.Drawing.Point(680, 321);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.dataGridView1.RowHeadersVisible = false;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
-            this.dataGridView1.Size = new System.Drawing.Size(290, 258);
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGridView1.Size = new System.Drawing.Size(387, 318);
             this.dataGridView1.TabIndex = 26;
             // 
             // Column1
             // 
             this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopRight;
-            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopRight;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle5;
             this.Column1.HeaderText = "Antivirus Vendor";
             this.Column1.Name = "Column1";
             this.Column1.ReadOnly = true;
@@ -199,8 +203,8 @@
             // Column2
             // 
             this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle6;
             this.Column2.HeaderText = "Detected";
             this.Column2.Name = "Column2";
             this.Column2.ReadOnly = true;
@@ -208,10 +212,10 @@
             // 
             // FileVirusChecker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.ClientSize = new System.Drawing.Size(812, 531);
+            this.ClientSize = new System.Drawing.Size(1083, 654);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.result);
@@ -221,8 +225,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Header);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FileVirusChecker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FileVirusChecker";
             this.Header.ResumeLayout(false);
             this.Header.PerformLayout();
--- OSCS/OSCS/WinForms/FileVirusChecker/FileVirusChecker.cs
+++ OSCS/OSCS/WinForms/FileVirusChecker/FileVirusChecker.cs
@@ -34,67 +34,76 @@ namespace OSCS.WinForms.FileVirusChecker
             {
                 fileName.Text = openFileDialog.FileName;
                 ScanFile = File.ReadAllBytes(openFileDialog.FileName);
+                //ScanFile = Encoding.ASCII.GetBytes(@"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
             }
         }
 
         private async Task FileCheck()
         {
-            //only can scan 4 per minute (public)
-            VirusTotal virusTotal = new VirusTotal("aaac0a1ce9f703c53077458ea6b54b0be48c7daae133ca4e7d37e08cf59be4e3");
-
-            //Use HTTPS instead of HTTP
-            virusTotal.UseTLS = true;
-
-            FileReport fileReport = await virusTotal.GetFileReportAsync(ScanFile);
-
-            bool hasFileBeenScannedBefore = fileReport.ResponseCode == FileReportResponseCode.Present;
-
-            if (hasFileBeenScannedBefore == true) //file report present, so report can be retrieved
-
+            if (ScanFile != null && ScanFile.Length > 0)
             {
-                int counter = fileReport.Positives; //Number of anti-virus vendors that had detected this file to be malicious
+                //only can scan 4 per minute (public)
+                VirusTotal virusTotal = new VirusTotal("aaac0a1ce9f703c53077458ea6b54b0be48c7daae133ca4e7d37e08cf59be4e3");
 
-                //ScanResult fileResult = await virusTotal.ScanFileAsync(ScanFile, "EICAR.txt");
-                //counter = PrintScan(fileReport); 
+                //Use HTTPS instead of HTTP
+                virusTotal.UseTLS = true;
 
-                //if there are 3 or more hits by anti-virus vendors  
-                if (counter >= 3)
+                FileReport fileReport = await virusTotal.GetFileReportAsync(ScanFile);
+
+                bool hasFileBeenScannedBefore = fileReport.ResponseCode == FileReportResponseCode.Present;
+
+                if (hasFileBeenScannedBefore == true) //file report present, so report can be retrieved
+
                 {
-                    result.Text = "Virus has been detected on this file! Do not open and delete immediately!";
-                    result.ForeColor = System.Drawing.Color.Red;
+                    int counter = fileReport.Positives; //Number of anti-virus vendors that had detected this file to be malicious
+
+                    //ScanResult fileResult = await virusTotal.ScanFileAsync(ScanFile, "EICAR.txt");
+                    //counter = PrintScan(fileReport); 
+
+                    //if there are 3 or more hits by anti-virus vendors  
+                    if (counter >= 3)
+                    {
+                        result.Text = "Virus has been detected on this file! Do not open it and delete it immediately!";
+                        result.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        result.Text = "The file is safe. No viruses were detected during scan.";
+                        result.ForeColor = System.Drawing.Color.Green;
+                    }
+
+                    //displaying results in grid view
+                    foreach (KeyValuePair<string, ScanEngine> scan in fileReport.Scans)
+                    {
+                        dataGridView1.Rows.Add(scan.Key, scan.Value.Detected);
+                        foreach (DataGridViewRow row in dataGridView1.Rows)
+                        {
+                            if (Convert.ToString(row.Cells[1].Value) == "True")
+                            {
+                                row.DefaultCellStyle.BackColor = Color.Red;
+                                row.DefaultCellStyle.ForeColor = Color.White;
+                            }
+                            else
+                            {
+                                row.DefaultCellStyle.BackColor = Color.Green;
+                                row.DefaultCellStyle.ForeColor = Color.Yellow;
+                            }
+
+                        }
+                    }
+
                 }
+
                 else
                 {
-                    result.Text = "The file is safe. No viruses were detected.";
-                    result.ForeColor = System.Drawing.Color.Green;
+                    ScanResult fileResult = await virusTotal.ScanFileAsync(ScanFile, "EICAR.txt");
+                    result.Text = "There are no reports retrieved for the file that you have just uploaded. We have sent the file to Virus Total for a scan.";
                 }
-
-                //displaying results in grid view
-                foreach (KeyValuePair<string, ScanEngine> scan in fileReport.Scans)
-                {
-                    dataGridView1.Rows.Add(scan.Key, scan.Value.Detected);
-                    foreach (DataGridViewRow row in dataGridView1.Rows)
-                    {
-                        if (Convert.ToString(row.Cells[1].Value) == "True")
-                        {
-                            row.DefaultCellStyle.BackColor = Color.Red;
-                            row.DefaultCellStyle.ForeColor = Color.White;
-                        }
-                        else
-                        {
-                            row.DefaultCellStyle.BackColor = Color.Green;
-                            row.DefaultCellStyle.ForeColor = Color.Yellow;
-                        }
-
-                    }
-                }
-
             }
-
             else
             {
-                ScanResult fileResult = await virusTotal.ScanFileAsync(ScanFile, "EICAR.txt");
-                result.Text = "There are no reports retrieved for the file you have just uploaded. We have sent the file to virus total for a scan.";
+                result.Text = "Please choose a file before starting the virus scan!";
+                result.ForeColor = System.Drawing.Color.Red;
             }
         }
 
@@ -105,7 +114,7 @@ namespace OSCS.WinForms.FileVirusChecker
             {
                 foreach (KeyValuePair<string, ScanEngine> scan in fileReport.Scans)
                 {
-                    Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
+                    //Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
                     if (scan.Value.Detected == true)
                     {
                         counter++;
